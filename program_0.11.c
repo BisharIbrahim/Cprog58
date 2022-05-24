e7e4f734ad70128266cad0da1933e5fe47791dd7
Bishar Ibrahim <45415723+BisharIbrahim@users.noreply.github.com>
2022-05-25 00:16:26 +1000

    Update program_0.11.c

--- program_0.11.c
+++ program_0.11.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-/*#include <PlaceHolder.h>*/
+#include "crypt.h"
 
 /******************************************************************************
  * Prototype Function
@@ -51,7 +51,7 @@ int main(){
                     break;
 
 				/*Case 2 should decrypt and decompres a file*/
-                case 2: compressFile();
+                case 2: decryptFile();
                     break;
 
                 case 3: adminOptions();
@@ -134,74 +134,18 @@ void adminOptions(){
  * Perfroms file encryption - Bishar
 ******************************************************************************/
 void encryptFile(){
-	char fname[20], ch;
-	FILE *fpts, *fptt;
+	char fname[100];
+	
 	
 	printf("\n\n"
     "************************************************************************\n"
-    "                         Encrypt a Text File\n"
+    "                        Encrpyt and Compress a Text File\n"
     "************************************************************************\n"); 	
-	
-	printf(" Input the name of file to encrypt : ");
-	scanf("%s",fname);	
 
-	fpts=fopen(fname, "r");
-	if(fpts==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		exit(1);
-	}
-	fptt=fopen("temp.txt", "w");
-	if(fptt==NULL)
-	{
-		printf(" Error in creation of file temp.txt ..!!");
-		fclose(fpts);
-		exit(2);
-	}
-	while(1)
-	{
-		ch=fgetc(fpts);
-		if(ch==EOF)
-		{
-			break;
-		}
-		else
-		{
-			ch=ch+100;
-			fputc(ch, fptt);
-		}
-	}
-	fclose(fpts);
-	fclose(fptt);
-	fpts=fopen(fname, "w");
-	if(fpts==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		exit(3);
-	}
-	fptt=fopen("temp.txt", "r");
-	if(fptt==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		fclose(fpts);
-		exit(4);
-	}
-	while(1)
-	{
-		ch=fgetc(fptt);
-		if(ch==EOF)
-		{
-			break;
-		}
-		else
-		{
-			fputc(ch, fpts);
-		}
-	}
-	printf(" File %s successfully encrypted ..!!\n\n", fname);
-	fclose(fpts);
-	fclose(fptt);
+	printf(" Input the name of file to encrypt and compress: ");
+	scanf("%s",fname);	
     
+    encrypt(fname);
 }
 
 /******************************************************************************
@@ -209,50 +153,21 @@ void encryptFile(){
 ******************************************************************************/
 void decryptFile(){
 
-	char ch, fname[20];
-	FILE *fpts, *fptt;
+	char fname[20];
 	
 	printf("\n\n"
     "************************************************************************\n"
     "                         Decrypt a Text File\n"
     "************************************************************************\n"); 		
-	
+
 	printf(" Input the name of file to decrypt : ");
 	scanf("%s",fname);	
 	
-	fpts=fopen(fname, "w");
-	if(fpts==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		exit(7);
-	}
-	fptt=fopen("temp.txt", "r");
-	if(fptt==NULL)
-	{
-		printf(" File does not exists or error in opening..!!");
-		fclose(fpts);
-		exit(9);
-	}
-	while(1)
-	{
-		ch=fgetc(fptt);
-		if(ch==EOF)
-		{
-			break;
-		}
-		else
-		{
-			ch=ch-100;
-			fputc(ch, fpts);
-		}
-	}
-	printf(" The file %s decrypted successfully..!!\n\n",fname);
-	fclose(fpts);
-	fclose(fptt);
+	decrypt(fname);
 
 }
 
-void compressFile(){
+/*void compressFile(){
      
     FILE f,g;
 
@@ -308,3 +223,4 @@ void compressFile(){
     printf("%s has been compressed\n", fname);
     exit(0);
 }
+*/
